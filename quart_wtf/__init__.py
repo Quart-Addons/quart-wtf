e45e8ed7c1ba3fbfe48464deed331c5332b425ab
Chris Rood <crood58@gmail.com>
2022-07-24 03:03:21 +0000

    Added import for file.

--- quart_wtf/__init__.py
+++ quart_wtf/__init__.py
@@ -2,6 +2,9 @@
 quart_form
 """
 from .csrf import CSRFProtect
+from .file import FileField, file_required, file_allowed
 from .form import QuartForm
+
+__all__ = ('CSRFProtect', 'FileField', 'file_required', 'file_allowed', 'QuartForm')
 
 __version__ = "0.0.1"
