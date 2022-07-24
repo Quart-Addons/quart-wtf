1c718f7176b56e22e190c36f8f76cfdc3e6edd09
Chris Rood <crood58@gmail.com>
2022-07-24 15:06:54 +0000

    Made file work with Quart.

--- tests/test_csrf_extension.py
+++ tests/test_csrf_extension.py
@@ -5,6 +5,7 @@ import pytest
 from quart import Blueprint, g, render_template_string
 
 from quart_wtf.csrf import CSRFError, CSRFProtect, generate_csrf
+from quart_wtf.form import QuartForm
 
 @pytest.fixture
 def app(app):
@@ -31,41 +32,41 @@ async def test_render_token(req_ctx):
     token = generate_csrf()
     assert await render_template_string("{{ csrf_token() }}") == token
 
-
-def test_protect(app, client, app_ctx):
-    response = client.post("/")
+@pytest.mark.asyncio
+async def test_protect(app, client, app_ctx):
+    response = await client.post("/")
     assert response.status_code == 400
     assert "The CSRF token is missing." in response.get_data(as_text=True)
 
     app.config["WTF_CSRF_ENABLED"] = False
-    assert client.post("/").get_data() == b""
+    assert await client.post("/").get_data() == b""
     app.config["WTF_CSRF_ENABLED"] = True
 
     app.config["WTF_CSRF_CHECK_DEFAULT"] = False
-    assert client.post("/").get_data() == b""
+    assert await client.post("/").get_data() == b""
     app.config["WTF_CSRF_CHECK_DEFAULT"] = True
 
-    assert client.options("/").status_code == 200
-    assert client.post("/not-found").status_code == 404
+    assert await client.options("/").status_code == 200
+    assert await client.post("/not-found").status_code == 404
 
-    response = client.get("/")
+    response = await client.get("/")
     assert response.status_code == 200
     token = response.headers["X-CSRF-Token"]
-    assert client.post("/", data={"csrf_token": token}).status_code == 200
-    assert client.post("/", data={"prefix-csrf_token": token}).status_code == 200
-    assert client.post("/", data={"prefix-csrf_token": ""}).status_code == 400
-    assert client.post("/", headers={"X-CSRF-Token": token}).status_code == 200
+    assert await client.post("/", data={"csrf_token": token}).status_code == 200
+    assert await client.post("/", data={"prefix-csrf_token": token}).status_code == 200
+    assert await client.post("/", data={"prefix-csrf_token": ""}).status_code == 400
+    assert await client.post("/", headers={"X-CSRF-Token": token}).status_code == 200
 
-
-def test_same_origin(client):
-    token = client.get("/").headers["X-CSRF-Token"]
-    response = client.post(
+@pytest.mark.asyncio
+async def test_same_origin(client):
+    token = await client.get("/").headers["X-CSRF-Token"]
+    response = await client.post(
         "/", base_url="https://localhost", headers={"X-CSRF-Token": token}
     )
     data = response.get_data(as_text=True)
     assert "The referrer header is missing." in data
 
-    response = client.post(
+    response = await client.post(
         "/",
         base_url="https://localhost",
         headers={"X-CSRF-Token": token, "Referer": "http://localhost/"},
@@ -73,7 +74,7 @@ def test_same_origin(client):
     data = response.get_data(as_text=True)
     assert "The referrer does not match the host." in data
 
-    response = client.post(
+    response = await client.post(
         "/",
         base_url="https://localhost",
         headers={"X-CSRF-Token": token, "Referer": "https://other/"},
@@ -81,7 +82,7 @@ def test_same_origin(client):
     data = response.get_data(as_text=True)
     assert "The referrer does not match the host." in data
 
-    response = client.post(
+    response = await client.post(
         "/",
         base_url="https://localhost",
         headers={"X-CSRF-Token": token, "Referer": "https://localhost:8080/"},
@@ -89,79 +90,79 @@ def test_same_origin(client):
     data = response.get_data(as_text=True)
     assert "The referrer does not match the host." in data
 
-    response = client.post(
+    response = await client.post(
         "/",
         base_url="https://localhost",
         headers={"X-CSRF-Token": token, "Referer": "https://localhost/"},
     )
     assert response.status_code == 200
 
-
-def test_form_csrf_short_circuit(app, client):
+@pytest.mark.asyncio
+async def test_form_csrf_short_circuit(app, client):
     @app.route("/skip", methods=["POST"])
-    def skip():
+    async def skip():
         assert g.get("csrf_valid")
         # don't pass the token, then validate the form
         # this would fail if CSRFProtect didn't run
-        form = FlaskForm(None)
+        form = QuartForm(None)
         assert form.validate()
 
-    token = client.get("/").headers["X-CSRF-Token"]
-    response = client.post("/skip", headers={"X-CSRF-Token": token})
+    token = await client.get("/").headers["X-CSRF-Token"]
+    response = await client.post("/skip", headers={"X-CSRF-Token": token})
     assert response.status_code == 200
 
-
-def test_exempt_view(app, csrf, client):
+@pytest.mark.asyncio
+async def test_exempt_view(app, csrf, client):
     @app.route("/exempt", methods=["POST"])
     @csrf.exempt
-    def exempt():
+    async def exempt():
         pass
 
-    response = client.post("/exempt")
+    response = await client.post("/exempt")
     assert response.status_code == 200
 
     csrf.exempt("test_csrf_extension.index")
-    response = client.post("/")
+    response = await client.post("/")
     assert response.status_code == 200
 
-
-def test_manual_protect(app, csrf, client):
+@pytest.mark.asyncio
+async def test_manual_protect(app, csrf, client):
     @app.route("/manual", methods=["GET", "POST"])
     @csrf.exempt
-    def manual():
+    async def manual():
         csrf.protect()
 
-    response = client.get("/manual")
+    response = await client.get("/manual")
     assert response.status_code == 200
 
-    response = client.post("/manual")
+    response = await client.post("/manual")
     assert response.status_code == 400
 
-
-def test_exempt_blueprint(app, csrf, client):
+@pytest.mark.asyncio
+async def test_exempt_blueprint(app, csrf, client):
     bp = Blueprint("exempt", __name__, url_prefix="/exempt")
     csrf.exempt(bp)
 
     @bp.route("/", methods=["POST"])
-    def index():
+    async def index():
         pass
 
     app.register_blueprint(bp)
-    response = client.post("/exempt/")
+    response = await client.post("/exempt/")
     assert response.status_code == 200
 
-
-def test_error_handler(app, client):
+@pytest.mark.asyncio
+async def test_error_handler(app, client):
     @app.errorhandler(CSRFError)
-    def handle_csrf_error(e):
+    async def handle_csrf_error(e):
         return e.description.lower()
 
-    response = client.post("/")
+    response = await client.post("/")
     assert response.get_data(as_text=True) == "the csrf token is missing."
 
-
-def test_validate_error_logged(client, monkeypatch):
-    from flask_wtf.csrf import logger
+@pytest.mark.asyncio
+async def test_validate_error_logged(client, monkeypatch):
+    from quart_wtf.csrf import logger
 
     messages = []
 
@@ -170,6 +171,6 @@ def test_validate_error_logged(client, monkeypatch):
 
     monkeypatch.setattr(logger, "info", assert_info)
 
-    client.post("/")
+    await client.post("/")
     assert len(messages) == 1
     assert messages[0] == "The CSRF token is missing."
