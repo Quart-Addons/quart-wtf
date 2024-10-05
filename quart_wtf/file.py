81dbd52946f0ad4b709b749f7afa31566fae7b36
Chris Rood <crood58@gmail.com>
2024-10-05 19:51:18 +0000

    Reverted back to long line.

--- quart_wtf/file.py
+++ quart_wtf/file.py
@@ -129,8 +129,7 @@ class FileSize:
             raise ValidationError(
                 self.message
                 or field.gettext(
-                    f"File must be between {self.min_size} and \
-                        {self.max_size} bytes."
+                    f"File must be between {self.min_size} and {self.max_size} bytes."
                 )
             )
 
